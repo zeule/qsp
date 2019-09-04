d15d6e7fbada35ffc8ec8c618ce76e9e14c821c0
Eugene Shalygin <eugene.shalygin@gmail.com>
2019-09-04 04:15:11 +0200

    Remove qgen

--- qgen/precompiled/precompiled.h
+++
@@ -1,9 +0,0 @@
-#ifndef __QGEN_PRECOMPILED_H__
-#define __QGEN_PRECOMPILED_H__
-
-#include <QtWidgets>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-
-#endif // __QGEN_PRECOMPILED_H__
