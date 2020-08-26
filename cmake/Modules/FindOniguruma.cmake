e5429532546ad6fec018ee6e4e42af1d72ce85fe
Eugene Shalygin <eugene.shalygin@gmail.com>
2020-08-26 17:49:50 +0200

    cmake tuning

--- cmake/Modules/FindOniguruma.cmake
+++ cmake/Modules/FindOniguruma.cmake
@@ -4,14 +4,15 @@
 
 
 if (NOT TARGET Oniguruma::Onig)
-	include(FindPackageHandleStandardArgs)
 	if (NOT WIN32)
 		# use pkg-config to get the directories and then use these values
-		include(FindPkgConfig)
-		message(VERBOSE "FindOnigurama: trying pkg-config")
-		pkg_check_modules(ONIGURUMA REQUIRED IMPORTED_TARGET GLOBAL oniguruma)
-		add_library(Oniguruma::Onig ALIAS PkgConfig::ONIGURUMA)
-		set(Oniguruma_FOUND True)
+		find_package(PkgConfig)
+		if (PkgConfig_FOUND)
+			message(VERBOSE "FindOnigurama: trying pkg-config")
+			pkg_check_modules(ONIGURUMA REQUIRED IMPORTED_TARGET GLOBAL oniguruma)
+			add_library(Oniguruma::Onig ALIAS PkgConfig::ONIGURUMA)
+			set(Oniguruma_FOUND True)
+		endif()
 	else(NOT WIN32)
 		find_library(_onig_lib NAMES onig)
 		find_path(_onig_include_dir NAMES oniguruma.h PATH_SUFFIXES oniguruma)
@@ -23,6 +24,7 @@ if (NOT TARGET Oniguruma::Onig)
 				IMPORTED_LOCATION "${_onig_lib}"
 			)
 		else(_onig_lib AND _onig_include_dir)
+		include(FindPackageHandleStandardArgs)
 		find_package_handle_standard_args(Oniguruma
 			REQUIRED_VARS _onig_lib _onig_include_dir)
 		endif(_onig_lib AND _onig_include_dir)
